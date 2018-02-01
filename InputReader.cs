afadec912a9833789eaed6f7d8d30e346d38a706
KrIsKa7a <kristiqn.ivanov@abv.bg>
2018-02-02 01:53:40 +0200

    Uploaded CommandInterpreter

---
+++ InputReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BashSoft
+{
+    public static class InputReader
+    {
+        private const string endCommand = "quit";
+        public static void StartReadingCommands()
+        {
+            OutputWriter.WriteMessage($"{SessionData.currentPath}> ");
+            string input = Console.ReadLine().Trim();
+
+            while (input != endCommand)
+            {
+                CommandInterpreter.InterpredCommand(input);
+                OutputWriter.WriteMessage($"{SessionData.currentPath}> ");
+                input = Console.ReadLine();
+
+                input = input.Trim();
+            }
+        }
+    }
+}
